a98a555c6fd9e9127a440c10e24ef9c02bdf60c3
Ninella Jenna Otte <ninella@hotmail.de>
2025-08-29 17:13:47 +0200

    Creating Basic Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 const LandingPage = () => {
-  return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Next Store - Tailwind is Working! 🎉
-      </h1>
-    </div>
-  );
+  return <div>LandingPage</div>;
 };
 
 export default LandingPage;
