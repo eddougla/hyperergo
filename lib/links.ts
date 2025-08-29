ac6fa4abc27c1d627837a7ce26d1646b3110a7e4
Ninella Jenna Otte <ninella@hotmail.de>
2025-08-29 22:37:58 +0200

    Navbar Links Dropdown

---
+++ lib/links.ts
@@ -0,0 +1,15 @@
+type NavLink = {
+  href: string;
+  label: string;
+};
+
+// Define the array of links
+export const links: NavLink[] = [
+  { href: "/", label: "home" },
+  { href: "/about", label: "about" },
+  { href: "/products", label: "products" },
+  { href: "/favorites", label: "favorites" },
+  { href: "/cart", label: "cart" },
+  { href: "/orders", label: "orders" },
+  { href: "/contact", label: "contact" },
+];
