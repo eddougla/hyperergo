b6cfb3fd36e7d143247d3f4d6100fd3e883f6beb
Ninella Jenna Otte <ninella@hotmail.de>
2025-08-30 00:45:33 +0200

    SectionTitle & EmptyList Component Setup

--- components/shared/EmptyList.tsx
+++ components/shared/EmptyList.tsx
@@ -1,5 +1,15 @@
-const EmptyList = () => {
-  return <div>No items found.</div>;
+import { cn } from "@/lib/utils";
+
+type EmptyListProps = {
+  heading?: string;
+  className?: string;
+};
+
+const EmptyList = ({
+  heading = "No items found.",
+  className,
+}: EmptyListProps) => {
+  return <h2 className={cn("text-xl", className)}>{heading}</h2>;
 };
 
 export default EmptyList;
--- components/shared/SectionTitle.tsx
+++ components/shared/SectionTitle.tsx
@@ -1,5 +1,18 @@
-const SectionTitle = ({ title }: { title: string }) => {
-  return <h2 className="text-3xl font-bold">{title}</h2>;
+import { Separator } from "@/components/ui/separator";
+
+type SectionTitleProps = {
+  text: string;
+};
+
+const SectionTitle = ({ text }: SectionTitleProps) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-medium tracking-wider capitalize mb-4">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 };
 
 export default SectionTitle;
