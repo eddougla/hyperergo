9bce908af003783114ed3f83d0067e431e7aeae9
Ninella Jenna Otte <ninella@hotmail.de>
2025-08-30 20:47:34 +0200

    Clerk User Icon Integration

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,21 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+import { LuUser } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
+
+const UserIcon = async () => {
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt="user icon"
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
+
+  return <LuUser className="w-6 h-6 bg-primary text-white rounded-full p-1" />;
 };
+
 export default UserIcon;
