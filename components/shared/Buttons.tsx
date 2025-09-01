fc1669676dc30def19aa0b7db3c897b7b8579139
Ninella Jenna Otte <ninella@hotmail.de>
2025-09-01 19:47:31 +0200

    Submit Button Component

--- components/shared/Buttons.tsx
+++ components/shared/Buttons.tsx
@@ -1,4 +1,38 @@
-const Buttons = () => {
-  return <div>Buttons</div>;
+"use client";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+// Reusable props for SubmitButton
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: "default" | "sm" | "lg" | "icon";
 };
-export default Buttons;
+
+export const SubmitButton = ({
+  className = "",
+  text = "Submit",
+  size = "lg",
+}: SubmitButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+      className={cn("capitalize", className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+};
